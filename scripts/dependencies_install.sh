18920ee56326b3344de41b350d8fdf75130fef42
Lewis Desforges <lewis.desforges@gmail.com>
2025-02-28 22:25:18 +0000

    feat: using kubespray instead

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -3,6 +3,9 @@
 # Make sure we have the right version of tar
 brew install gnu-tar
 
+# kubespray is fussy about the version of ansible it uses
+pipx install ansible-core==2.16.14
+
 # Install the required Ansible roles
 ansible-galaxy role install geerlingguy.containerd geerlingguy.kubernetes
 ansible-galaxy collection install community.general ansible.posix prometheus.prometheus kubernetes.core
